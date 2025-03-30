5da1c26830378ceb2183d376e69934ae1abc5a4f
Leo <36726619+leostricak22@users.noreply.github.com>
2025-03-30 23:34:06 +0200

    fix: expo router handling #2

--- apps/frontend/src/app/auth/callback.tsx
+++ apps/frontend/src/app/auth/callback.tsx
@@ -1,27 +1,32 @@
-import { useEffect } from 'react';
-import {UnknownOutputParams, useLocalSearchParams, useRouter} from 'expo-router';
+import { useEffect, useState } from "react";
+import { UnknownOutputParams, useLocalSearchParams, useRouter } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Loader from "@/src/components/loader/Loader";
 
 export default function CallbackScreen() {
     const params = useLocalSearchParams();
     const router = useRouter();
+    const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
-        const checkToken = async (params:UnknownOutputParams) => {
+        setIsMounted(true);
+    }, []);
+
+    useEffect(() => {
+        if (!isMounted) return;
+
+        const checkToken = async (params: UnknownOutputParams) => {
             if (params.token) {
                 await AsyncStorage.setItem("token", params.token as string);
-                router.push("/dashboard");
+                router.replace("/dashboard");
             } else {
                 let errorMessage = params.error || "";
-                router.push(`/auth/login?error=${errorMessage}`);
+                router.replace(`/auth/login?error=${errorMessage}`);
             }
-        }
+        };
 
         checkToken(params).then(r => r);
-    }, [params]);
+    }, [params, isMounted]);
 
-    return (
-        <Loader />
-    );
+    return <Loader />;
 }
