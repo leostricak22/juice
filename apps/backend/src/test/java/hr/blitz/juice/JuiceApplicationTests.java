0ca07e9719d9a76ea359f6ccaefd7c1b66058840
leostricak22 <stricak.leo@gmail.com>
2025-05-05 01:26:48 +0200

    fix: contextLoads test

--- apps/backend/src/test/java/hr/blitz/juice/JuiceApplicationTests.java
+++ apps/backend/src/test/java/hr/blitz/juice/JuiceApplicationTests.java
@@ -3,7 +3,7 @@ package hr.blitz.juice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = JuiceApplicationTests.class)
 class JuiceApplicationTests {
 
 	@Test
